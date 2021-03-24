76c80263109826df272527aeb87db20edc84da74
KanaFarira <kanafarira@gmail.com>
2021-03-24 19:47:09 +0700

    coba 1

---
+++ belajar_laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    public function index(){
+        return "haii ini method index, dalam controller management";
+    }
+
+}
--- belajar_laravel/routes/web.php
+++ belajar_laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::resource('/user', 'App\Http\Controllers\ManagementUserController');
