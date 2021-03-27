bedd7018d5bc894004bf1ca660c2e3c77eaa7e87
KanaFarira <kanafarira@gmail.com>
2021-03-27 14:25:44 +0700

    home

--- belajar_laravel/resources/views/app.blade.php
+++ belajar_laravel/resources/views/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale())}}">
 <head>
     <meta charset="UTF-8">
@@ -9,12 +9,8 @@
 <body>
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-4"> HALAMAN HOME </h1>
-            <p class="lead"> Halaman ini merupakan halaman home </p>
+            @yield('content')
         </div>
     </div>
 </body>
-</html>
-
-
-
+</html> -->
