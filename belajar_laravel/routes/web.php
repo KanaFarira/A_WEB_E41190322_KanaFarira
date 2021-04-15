406ec3ae745eb25d02c15235ebc55f7a0bdf4f65
KanaFarira <kanafarira@gmail.com>
2021-04-15 10:57:24 +0700

    coba minggu 6 eror

--- belajar_laravel/routes/web.php
+++ belajar_laravel/routes/web.php
@@ -23,3 +23,10 @@ Route::resource('/user', 'App\Http\Controllers\ManagementUserController');
 // Route::get ("/home", function(){
 //         return view("home");
 // });
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
